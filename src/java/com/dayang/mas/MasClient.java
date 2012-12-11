8040c078d9ed801022de228259e5aa8d524a978b
lvjian <lvjian@dayang.com.cn>
2012-12-11 16:44:01 +0800

    添加log4j, 支持日志输出
    添加更详细的日志

--- src/java/com/dayang/mas/MasClient.java
+++ src/java/com/dayang/mas/MasClient.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.ParseException;
+import org.apache.log4j.Logger;
 
 import com.jasson.mas.api.sms.SmsApiClient;
 import com.jasson.mas.api.sms.SmsApiClientHandler;
@@ -22,28 +23,50 @@ import com.jasson.mas.api.smsapi.SmsType;
 
 public class MasClient {
 	
+	static Logger log = Logger.getLogger(MasClient.class);
+	
 	static SmsApiClient smsApiClient = null;
 	
 	// SmsApiClientHandlerImpl是由自己实现的
 	static SmsApiClientHandler smsHandler = new SmsApiClientHandlerImpl();
 	
 	public static void main(String[] args) throws ParseException {
+		log.info("start mas invoker app...");
+		
+		if(args == null || args.length == 0) {
+			log.info("non-arg, print Help by default");
+			printHelp();
+			System.exit(0);
+			return;
+		}
+		
+		log.info("parsing args...");
+		log.info("argv count:" + args.length);
 		CommandLine cl = parseCmd(args);
 		
 		if(cl.hasOption("h")) {
+			log.info("has --help arg, print Help");
 			printHelp();
 			System.exit(1);
 			return;
 		}
 		
+		log.info("reading args...");
+		
 		String ip = cl.getOptionValue("ip");
+		log.info("-ip:" + ip);
+		
 		String strPort = cl.getOptionValue("port");
+		log.info("-port:" + strPort);
 		int port = Integer.parseInt(strPort);
 		
 		String appId = cl.getOptionValue("app");
+		log.info("-appId:" + appId);
 		String pwd = cl.getOptionValue("pwd", "");
+		log.info("-pwd:" + pwd);
 		
 		String mobile = cl.getOptionValue("mobile");
+		log.info("-mobile:" + mobile);
 		if(mobile == null) {
 			System.out.println("mobile is required.");
 			System.exit(400);			
@@ -52,17 +75,23 @@ public class MasClient {
 		String[] mobiles = mobile.split(",");
 		
 		String message = cl.getOptionValue("msg", "");
+		log.info("-msg:" + message);
+		
 		String xcode = cl.getOptionValue("code", "1111111");
+		log.info("-code:" + xcode);
+		
 		String strPriority = cl.getOptionValue("priority", "1");
+		log.info("-priority:" + strPriority);
 		int priority = Integer.parseInt(strPriority);
 		
 		try {
+			log.info("create a SmsApiClient");
 			smsApiClient = getSmsApiClient(smsHandler, ip, port,
 					appId, pwd);
 			
 			// 获取网关连接状态(Connect:连接正常, Disconnect:断连, NotConnect:没有连接, Other:其他)
 			if(!isConnected(smsApiClient)) {
-				System.out.println("网关未连接");
+				log.error("网关未连接");
 				System.exit(404);
 				return;
 			}
@@ -85,11 +114,12 @@ public class MasClient {
 			
 			SmsSendResponse smsSendResponse = smsApiClient
 					.sendSms(smsSendRequest);
-			System.out.println("提交成功,requestID:" + smsSendResponse.requestID);
+			log.info("提交成功,requestID:" + smsSendResponse.requestID);
 			
 		} catch (Exception e) {
+			log.error("API短信客户端调用失败:" + e.getMessage());
+			log.error(e);
 			
-			System.out.println("API短信客户端调用失败:" + e.getMessage());
 			System.exit(500);
 			return;
 		}
