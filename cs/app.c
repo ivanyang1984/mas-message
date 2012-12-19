6f04ecec80ccc4527d8439612751e1b19f3b38f7
lvjian <lvjian@dayang.com.cn>
2012-12-19 10:02:57 +0800

    invoke jar directly.

--- cs/app.c
+++ cs/app.c
@@ -2,7 +2,8 @@
 
 int main(int argc, const char *argv[])
 {
-	int ret = system("../run -h");	
-	printf("The result: %d \n", ret);
+	int ret = system("java -jar /home/lvjian/sts/mas-message/bin/jar/mas-message.0.1.0.jar -h");	
+	printf("The help result: %d \n", ret);
+	
 	return 0;
 }
